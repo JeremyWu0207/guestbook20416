fa93f32558726173abd638e2c37e3a81c109247a
Jeremy Wu <jeremyccwu@gmail.com>
2022-12-30 10:30:30 +0800

    done

--- web/views.py
+++ web/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Message
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -18,4 +19,8 @@ class MessageDetail(DetailView):
 class MessageCreate(CreateView):
     model = Message
     fields = ['user', 'subject', 'content']     
+    success_url = reverse_lazy('msg_list')
+
+class MessageDelete(LoginRequiredMixin, DeleteView):
+    model = Message
     success_url = reverse_lazy('msg_list')
